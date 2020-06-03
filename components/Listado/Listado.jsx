2a757c4a3925a9dd98e3849fdc96f278706df7f7
Ale R. Buteler <alebuteler@gmail.com>
2020-06-03 15:27:47 -0300

    Buscando data para server side rendering con node-fetch.

--- components/Listado/Listado.jsx
+++ components/Listado/Listado.jsx
@@ -1,11 +1,11 @@
 import { Grilla, Etiquetas } from '../index';
 
-function Listado () {
+function Listado ({data}) {
   return (<div>
     <h1>Grilla here.</h1>
     <Etiquetas />
-    <Grilla />
-
+    <Grilla data={data} />
+    <h3>{data && data.articles && data.articles[3].headlines.basic}</h3>
     <button>Mas notas</button>
   </div>);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
+import fetch from 'node-fetch';
 import { Listado } from "../components/index";
 
-export default function Index() {
+function Index ({ articles }) {
   return (<div>
-
     <div>Banner top</div>
     <div className="content-container">
-      <Listado />
+      <Listado data={articles} />
       <div>Banner aside</div>
     </div>
   </div>);
 }
+
+export async function getServerSideProps () {
+  const res = await fetch('https://api-test-ln.herokuapp.com/articles');
+  const articles = await res.json();
+  return {
+    props: {
+      articles,
+    }
+  };
+}
+
+export default Index;
